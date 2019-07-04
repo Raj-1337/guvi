bfe6f2483b73eb4b33ec9b728003739d83c54005
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-04 22:56:39 +0530

    Update p1_6.py

--- p1_6.py
+++ p1_6.py
@@ -1,1 +1,14 @@
 
+def isomorphic(s):
+  a = list(set([i for i in s]))
+  r = []
+  for i in a:
+      r.append(s.count(i))
+  return r
+n = input().split()
+t1 = isomorphic(n[0])
+t2 = isomorphic(n[1])
+if sorted(t1) == sorted(t2):
+  print("yes")
+else:
+  print("no")
