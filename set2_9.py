41b194540f10fb03a46300f6e235df84e0176232
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-04 11:23:36 +0530

    Update set2_9.py

--- set2_9.py
+++ set2_9.py
@@ -1,1 +1,14 @@
 
+n = list(map(int, input().split()))
+r = []
+for i in range(n[0], n[-1]):
+  t = str(i)
+  l = len(t)
+  c = 0
+  for j in t:
+    j = int(j)
+    c += pow(j, l)
+  if c == i:
+    r.append(i)
+for i in r:
+  print(i, end= " ")
