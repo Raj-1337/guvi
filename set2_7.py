38fa50d438a53ed75fc77d560fa5dbb0ef909213
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-04 10:49:49 +0530

    Update set2_7.py

--- set2_7.py
+++ set2_7.py
@@ -1,1 +1,12 @@
 
+n = list(map(int, input().split()))
+x = []
+for i in range(n[0], n[1]):
+  flag = True
+  for j in range(2, i):
+    if i % j == 0:
+      flag = False
+      break
+    if flag:
+      x.append(j)
+print(' '.join(x))
