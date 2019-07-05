fca02a19fd24edbd6c78eb019c303c841da477b9
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-05 21:56:33 +0530

    Create m1_9.py

---
+++ m1_9.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+k = input()
+x = list(map(int, input().split()))
+r = 0
+c = 10000
+for i in permutations(x, 2):
+    if sum(i) == 0:
+        r = i
+        break
+    elif sum(i) < c:
+        r = i
+print(r[0], r[1])
