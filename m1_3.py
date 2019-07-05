be39b2d7f98a59fe6ce10b703e80f657f6781027
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-05 14:36:45 +0530

    Update m1_3.py

--- m1_3.py
+++ m1_3.py
@@ -1,1 +1,11 @@
-
+k = input()
+x = list(map(int, input().split()))
+r = []
+for i in range(len(x)):
+	if i == x[i]:
+		r.append(i)
+if r:
+	for i in sorted(r):
+		print(i, end=" ")
+else:
+	print(-1)
