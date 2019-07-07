a6cde37a265b3cc70ef923313e4d39f673f99cdd
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-07 19:28:02 +0530

    Update m3_1.py

--- m3_1.py
+++ m3_1.py
@@ -1,23 +1,23 @@
-p, q = map(int, input().split())
+a, b = map(int, input().split())
 r = []
-for i in range(p):
+for i in range(a):
     z = []
     y = input().split()
     for s in y:
         z.append((s, True))
     r.append(z)
-for i in range(p):
-    for j in range(q):
+for i in range(a):
+    for j in range(b):
         if r[i][j] == ('0', True):
-            z = []
-            for k in range(m):
-                z.append(('0', False))
-            r[i] = z
-            for a in r:
-                k[a] = ('0', False)
-for i in r:
-    t = []
-    for j in i:
-        t.append(j[0])
-    print(" ".join(t))
+            x = []
+            for k in range(b):
+                x.append(('0', False))
+            r[i] = x
+            for k in r:
+                k[j] = ('0', False)
+for l in r:
+    x = []
+    for y in l:
+        x.append(y[0])
+    print(" ".join(x))
 
