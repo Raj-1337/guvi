272584122738dba732c96dc0a0a7bb5927f565f9
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-11 14:04:37 +0530

    Update pro3_1.py

--- pro3_1.py
+++ pro3_1.py
@@ -1,23 +1,8 @@
-from statistics import mean
-from itertools import permutations
+
 k = int(input())
 x = list(map(int, input().split()))
-flag = True
-for i in permutations(x, len(x)):
-    if flag:
-        for j in range(1, len(i)):
-            t1 = i[:j]
-            t2 = i[j:]
-            print("loop:", t1, t2)
-            a1 = mean(t1)
-            a2 = mean(t2)
-            print("avg:", a1, a2)
-            if a1 == a2:
-                print('yes')
-                flag = False
-                break
-    else:
-        break
-    print("\n")
+n = int(len(x)/2)
+if sum(x[:n])//len(x[:n]) == sum(x[n:])//len(x[n:]):
+    print('yes')
 else:
     print('no')
