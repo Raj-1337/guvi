f8e73cf77dec28bade2ed5cc397bffd892a275f8
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-12 19:37:19 +0530

    Update pro4_7.py

--- pro4_7.py
+++ pro4_7.py
@@ -1,19 +1,11 @@
 k = input()
 x = list(map(int, input().split()))
 r = 0
-if len(x) == 1:
-    print(1)
-    exit(0)
-elif len(x) == 2:
-    if x[0] == x[1]:
-        print(0)
-        exit(0)
-    else:
-        print(2)
-        exit(0)
+x.insert(0, x[-1])
+x.append(x[1])
+if len(x) < 3:
+    print(0)
 else:
-    x.insert(0, x[-1])
-    x.append(x[1])
     for i in range(1, len(x)-1):
         if (x[i] > x[i-1] and x[i] > x[i+1]) or (x[i] < x[i-1] and x[i] < x[i+1]):
             r += 1
