3381fb1190630705ec06a0fa1bc79e4adf0fd970
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-13 22:06:29 +0530

    Update pro5_2.py

--- pro5_2.py
+++ pro5_2.py
@@ -2,5 +2,7 @@ n, k = map(int, input().split())
 x = list(map(int, input().split()))
 r = []
 for i in range(0, len(x)+1, k):
+    if i+k > len(x):
+        break
     r.append(min(x[i:i+k]))
 print(max(r))
