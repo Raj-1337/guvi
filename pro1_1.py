f4a71699a6089b69e041b2ad28105e028571cbfb
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-14 21:45:50 +0530

    Update pro1_1.py

--- pro1_1.py
+++ pro1_1.py
@@ -2,16 +2,13 @@ n = int(input())
 x = []
 for _ in range(n):
     x.append(input())
-r = []
-for i in range(len(x)):
-    for j in range(i+1, len(x)):
-        t = ''
-        c = 0
-        while x[i][c] == x[j][c]:
-            t += x[i][c]
-            c += 1
-            if len(x[i]) == c or len(x[j]) == c:
-                break
-        if t:
-            r.append(t)
-print(max(r, key=(lambda a: len(a))))
+r = ''
+c = 0
+for i in x[0]:
+    for j in x[1:]:
+        if j[c] != i:
+            break
+    else:
+        r += i
+    c += 1
+print(r)
