30f22c2fc75da2fd0b1d7e5802873a4786afece2
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-14 21:56:22 +0530

    Update pro1_1.py

--- pro1_1.py
+++ pro1_1.py
@@ -2,13 +2,17 @@ n = int(input())
 x = []
 for _ in range(n):
     x.append(input())
-r = ''
-c = 0
+r, c, flag = '', 0, False
 for i in x[0]:
     for j in x[1:]:
+        if len(j) == c:
+            flag = True
+            break
         if j[c] != i:
             break
     else:
         r += i
+    if flag:
+        break
     c += 1
 print(r)
