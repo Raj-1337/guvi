f975e3c0da246a681e663068cf876d4b45f180fe
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-17 22:19:17 +0530

    Update m11_8.py

--- m11_8.py
+++ m11_8.py
@@ -1,6 +1,9 @@
 x = [int(i) for i in input()]
+if len(x) == 1:
+    print(x[0] * x[0])
+    exit(0)
 x.append(x[0])
 c = 0
-for i in range(len(x)-1):
-    c += pow(x[i], x[i+1])
+for i in range(len(x) - 1):
+    c += pow(x[i], x[i + 1])
 print(c)
