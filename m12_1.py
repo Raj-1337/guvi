d72f0fbd3da1aa65a6b722d3693aa2aaf6c01f84
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-07-17 09:52:19 +0530

    Update m12_1.py

--- m12_1.py
+++ m12_1.py
@@ -3,6 +3,8 @@ r = []
 for i in range(n):
     r.append(list(map(int, input().split())))
 c = 0
-for i in range(n):
-    c += r[i][i]
+for i in range(n-1, 0, -1):
+    c += r[i][i-1]
+    if i == 1:
+        break
 print(c)
