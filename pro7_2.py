e84e32ab5e99447bcccab4a7b0c7045cd489808d
Raj-1337 <36556538+Raj-1337@users.noreply.github.com>
2019-08-02 13:18:16 +0530

    Create pro7_2.py

---
+++ pro7_2.py
@@ -0,0 +1,10 @@
+x = input()
+r = ""
+for i in range(len(x)):
+    for j in range(len(x), i-1, -1):
+        t = x[i:j]
+        if len(t) <= 1:
+            continue
+        elif t == t[::-1]:
+            print(len(x)-len(t))
+            exit(0)
